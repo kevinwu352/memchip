c7135cc749c88846c5589d9fdf117260bae97f64
kevin <kevinwu352@gmail.com>
2025-10-21 11:27:41 +0800

    setup home page

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import '/l10n/localizations.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,9 +7,30 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')),
       floatingActionButton: FloatingActionButton(onPressed: () {}, child: Icon(Icons.run_circle)),
-      body: Text('--'),
+      body: SizedBox.expand(
+        child: Column(
+          children: [
+            Container(color: Colors.blue, height: 300),
+            Expanded(
+              child: MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 40,
+                    childAspectRatio: 2,
+                  ),
+                  itemCount: 20,
+                  itemBuilder: (context, index) => Container(color: Colors.red),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
